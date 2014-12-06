a29e0f7c4875f90690126b92510954f464b365ca
Kirill <kolbasisha@gmail.com>
2014-12-06 14:41:40 +0600

    Added textures for fists and running string.

--- core/src/main/java/com/catinthedark/ld31/impl/common/Assets.java
+++ core/src/main/java/com/catinthedark/ld31/impl/common/Assets.java
@@ -1,5 +1,7 @@
 package com.catinthedark.ld31.impl.common;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
 public class Assets {
@@ -18,10 +20,15 @@ public class Assets {
 
     public static class Textures implements Initable {
 
+        public Texture fistLeftTex;
+        public Texture fistTopTex;
+        public Texture runningStringTex;
 
         @Override
         public void init() {
-
+            fistLeftTex = new Texture(Gdx.files.internal("texture/fist_left.png"));
+            fistTopTex = new Texture(Gdx.files.internal("texture/fist_top.png"));
+            runningStringTex = new Texture(Gdx.files.internal("texture/running_string.png"));
         }
     }
 
