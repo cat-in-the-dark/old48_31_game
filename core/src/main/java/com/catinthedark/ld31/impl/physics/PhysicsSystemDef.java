47b859f17acded92c5991ddd4d9f566cc31cf986
over <overln2@gmail.com>
2014-12-07 16:22:32 +0600

    Fixed attack time

--- core/src/main/java/com/catinthedark/ld31/impl/physics/PhysicsSystemDef.java
+++ core/src/main/java/com/catinthedark/ld31/impl/physics/PhysicsSystemDef.java
@@ -112,7 +112,8 @@ public class PhysicsSystemDef extends AbstractSystemDef {
             float camPosX = gameShared.cameraPosX.get().x;
             if (playerBody.getPosition().x * 32 + Constants.GAME_RECT.x < camPosX) {
                 playerBody.setLinearVelocity(0, playerBody.getLinearVelocity().y);
-                playerBody.getPosition().set((camPosX - Constants.GAME_RECT.x) / 32 + 1, playerBody.getPosition().y);
+                playerBody.getPosition().set((camPosX - Constants.GAME_RECT.x) / 32 + 1,
+                    playerBody.getPosition().y);
             }
 
             Camera cam = new OrthographicCamera(755 / 32, 520 / 32);
@@ -178,7 +179,7 @@ public class PhysicsSystemDef extends AbstractSystemDef {
                 if (playerBody.getPosition().x * 32 + Constants.GAME_RECT.x > camPosX) {
                     if (playerBody.getLinearVelocity().x > -10) {
                         playerBody.applyLinearImpulse(Constants.WALKING_FORCE_LEFT, new Vector2(0,
-                                0), true);
+                            0), true);
                     }
                 }
             } else {
@@ -197,127 +198,129 @@ public class PhysicsSystemDef extends AbstractSystemDef {
 
 
         void handleDaddyAttack(DaddyAttack attack) {
-            System.out.println("Physics:" + attack);
-            if (attack.direction == AttackDirection.BY_COL) {
-                float camPosX = gameShared.cameraPosX.get().x;
-                //normalize
-                camPosX -= Constants.GAME_RECT.getWidth() / 2;
-                if (camPosX < 0)
-                    camPosX = 0;
+            defer(() -> {
+                System.out.println("Physics:" + attack);
+                if (attack.direction == AttackDirection.BY_COL) {
+                    float camPosX = gameShared.cameraPosX.get().x;
+                    //normalize
+                    camPosX -= Constants.GAME_RECT.getWidth() / 2;
+                    if (camPosX < 0)
+                        camPosX = 0;
 
-                float attackX = attack.pos.x;
-                attackX = attackX - attackX % 32;
-                attackX /= 32;
-                System.out.println("cam_x:" + camPosX);
-                System.out.println("attackX:" + attackX);
+                    float attackX = attack.pos.x;
+                    attackX = attackX - attackX % 32;
+                    attackX /= 32;
+                    System.out.println("cam_x:" + camPosX);
+                    System.out.println("attackX:" + attackX);
 
-                List<Long> blocksForRemove = new ArrayList<>();
-                for (Map.Entry<Long, Body> kv : blocks.entrySet()) {
-                    //System.out.println("bpos:" + kv.getValue().getPosition());
-                    if (Math.abs(camPosX / 32 + attackX - kv.getValue().getPosition().x) < 1) {
-                        world.destroyBody(kv.getValue());
-                        blocksForRemove.add(kv.getKey());
-                        blockDestroyed.write(kv.getKey());
-                        System.out.println("desttoy block:" + kv.getValue().getPosition());
+                    List<Long> blocksForRemove = new ArrayList<>();
+                    for (Map.Entry<Long, Body> kv : blocks.entrySet()) {
+                        //System.out.println("bpos:" + kv.getValue().getPosition());
+                        if (Math.abs(camPosX / 32 + attackX - kv.getValue().getPosition().x) < 1) {
+                            world.destroyBody(kv.getValue());
+                            blocksForRemove.add(kv.getKey());
+                            blockDestroyed.write(kv.getKey());
+                            System.out.println("desttoy block:" + kv.getValue().getPosition());
+                        }
                     }
-                }
-                blocksForRemove.forEach((id) -> blocks.remove(id));
+                    blocksForRemove.forEach((id) -> blocks.remove(id));
 
-                List<Integer> jumpersForRemove = new ArrayList<>();
-                for (Map.Entry<Integer, Body> kv : jumpers.entrySet()) {
-                    if (Math.abs(camPosX / 32 + attackX - kv.getValue().getPosition().x) < 1) {
-                        world.destroyBody(kv.getValue());
-                        jumpersForRemove.add(kv.getKey());
-                        jumpersDestroyed.write(kv.getKey(), () ->{
-                            gameShared.jumpers.free(kv.getKey());
-                        });
+                    List<Integer> jumpersForRemove = new ArrayList<>();
+                    for (Map.Entry<Integer, Body> kv : jumpers.entrySet()) {
+                        if (Math.abs(camPosX / 32 + attackX - kv.getValue().getPosition().x) < 1) {
+                            world.destroyBody(kv.getValue());
+                            jumpersForRemove.add(kv.getKey());
+                            jumpersDestroyed.write(kv.getKey(), () -> {
+                                gameShared.jumpers.free(kv.getKey());
+                            });
+                        }
                     }
-                }
-                jumpersForRemove.forEach((id) -> jumpers.remove((Object) id));
+                    jumpersForRemove.forEach((id) -> jumpers.remove((Object) id));
 
-                List<Integer> walkersForRemove = new ArrayList<>();
-                for (Map.Entry<Integer, Body> kv : walkers.entrySet()) {
-                    if (Math.abs(camPosX / 32 + attackX - kv.getValue().getPosition().x) < 1) {
-                        world.destroyBody(kv.getValue());
-                        walkersForRemove.add(kv.getKey());
-                        walkersDestroyed.write(kv.getKey(), () ->{
-                            gameShared.walkers.free(kv.getKey());
-                        });
+                    List<Integer> walkersForRemove = new ArrayList<>();
+                    for (Map.Entry<Integer, Body> kv : walkers.entrySet()) {
+                        if (Math.abs(camPosX / 32 + attackX - kv.getValue().getPosition().x) < 1) {
+                            world.destroyBody(kv.getValue());
+                            walkersForRemove.add(kv.getKey());
+                            walkersDestroyed.write(kv.getKey(), () -> {
+                                gameShared.walkers.free(kv.getKey());
+                            });
+                        }
                     }
-                }
-                walkersForRemove.forEach((id) -> walkers.remove((Object) id));
+                    walkersForRemove.forEach((id) -> walkers.remove((Object) id));
 
-                List<Integer> shootersForRemove = new ArrayList<>();
-                for (Map.Entry<Integer, Body> kv : shooters.entrySet()) {
-                    if (Math.abs(camPosX / 32 + attackX - kv.getValue().getPosition().x) < 1) {
-                        world.destroyBody(kv.getValue());
-                        shootersForRemove.add(kv.getKey());
-                        shootersDestroyed.write(kv.getKey(), () ->{
-                            gameShared.shooters.free(kv.getKey());
-                        });
+                    List<Integer> shootersForRemove = new ArrayList<>();
+                    for (Map.Entry<Integer, Body> kv : shooters.entrySet()) {
+                        if (Math.abs(camPosX / 32 + attackX - kv.getValue().getPosition().x) < 1) {
+                            world.destroyBody(kv.getValue());
+                            shootersForRemove.add(kv.getKey());
+                            shootersDestroyed.write(kv.getKey(), () -> {
+                                gameShared.shooters.free(kv.getKey());
+                            });
+                        }
                     }
-                }
-                shootersForRemove.forEach((id) -> shooters.remove((Object) id));
+                    shootersForRemove.forEach((id) -> shooters.remove((Object) id));
 
 //            int delta = ((int) gameShared.cameraPosX.get().x) - ((int) gameShared.cameraPosX.get
 //                ().x) % ((int) Constants.GAME_RECT.width);
 //            System.out.print("delta: " + delta);
-            } else {
-                float attackY = Constants.GAME_RECT.height - attack.pos.y;
-                attackY -= 10;
-                attackY = attackY - attackY % 32;
-                attackY /= 32;
-                System.out.println("attackY:" + attackY);
+                } else {
+                    float attackY = Constants.GAME_RECT.height - attack.pos.y;
+                    attackY -= 10;
+                    attackY = attackY - attackY % 32;
+                    attackY /= 32;
+                    System.out.println("attackY:" + attackY);
 
-                List<Long> forRemove = new ArrayList<>();
-                for (Map.Entry<Long, Body> kv : blocks.entrySet()) {
-                    //System.out.println("bpos:" + kv.getValue().getPosition());
-                    if (Math.abs(attackY - kv.getValue().getPosition().y) < 1) {
-                        world.destroyBody(kv.getValue());
-                        forRemove.add(kv.getKey());
-                        blockDestroyed.write(kv.getKey());
-                        System.out.println("desttoy block:" + kv.getValue().getPosition());
+                    List<Long> forRemove = new ArrayList<>();
+                    for (Map.Entry<Long, Body> kv : blocks.entrySet()) {
+                        //System.out.println("bpos:" + kv.getValue().getPosition());
+                        if (Math.abs(attackY - kv.getValue().getPosition().y) < 1) {
+                            world.destroyBody(kv.getValue());
+                            forRemove.add(kv.getKey());
+                            blockDestroyed.write(kv.getKey());
+                            System.out.println("desttoy block:" + kv.getValue().getPosition());
+                        }
                     }
-                }
 
-                forRemove.forEach((id) -> blocks.remove(id));
+                    forRemove.forEach((id) -> blocks.remove(id));
 
-                List<Integer> jumpersForRemove = new ArrayList<>();
-                for (Map.Entry<Integer, Body> kv : jumpers.entrySet()) {
-                    if (Math.abs(attackY - kv.getValue().getPosition().y) < 1) {
-                        world.destroyBody(kv.getValue());
-                        jumpersForRemove.add(kv.getKey());
-                        jumpersDestroyed.write(kv.getKey(), () ->{
-                            gameShared.jumpers.free(kv.getKey());
-                        });
+                    List<Integer> jumpersForRemove = new ArrayList<>();
+                    for (Map.Entry<Integer, Body> kv : jumpers.entrySet()) {
+                        if (Math.abs(attackY - kv.getValue().getPosition().y) < 1) {
+                            world.destroyBody(kv.getValue());
+                            jumpersForRemove.add(kv.getKey());
+                            jumpersDestroyed.write(kv.getKey(), () -> {
+                                gameShared.jumpers.free(kv.getKey());
+                            });
+                        }
                     }
-                }
-                jumpersForRemove.forEach((id) -> jumpers.remove((Object) id));
+                    jumpersForRemove.forEach((id) -> jumpers.remove((Object) id));
 
-                List<Integer> walkersForRemove = new ArrayList<>();
-                for (Map.Entry<Integer, Body> kv : walkers.entrySet()) {
-                    if (Math.abs(attackY - kv.getValue().getPosition().y) < 1) {
-                        world.destroyBody(kv.getValue());
-                        walkersForRemove.add(kv.getKey());
-                        walkersDestroyed.write(kv.getKey(), () ->{
-                            gameShared.walkers.free(kv.getKey());
-                        });
+                    List<Integer> walkersForRemove = new ArrayList<>();
+                    for (Map.Entry<Integer, Body> kv : walkers.entrySet()) {
+                        if (Math.abs(attackY - kv.getValue().getPosition().y) < 1) {
+                            world.destroyBody(kv.getValue());
+                            walkersForRemove.add(kv.getKey());
+                            walkersDestroyed.write(kv.getKey(), () -> {
+                                gameShared.walkers.free(kv.getKey());
+                            });
+                        }
                     }
-                }
-                walkersForRemove.forEach((id) -> walkers.remove((Object) id));
+                    walkersForRemove.forEach((id) -> walkers.remove((Object) id));
 
-                List<Integer> shootersForRemove = new ArrayList<>();
-                for (Map.Entry<Integer, Body> kv : shooters.entrySet()) {
-                    if (Math.abs(attackY - kv.getValue().getPosition().y) < 1) {
-                        world.destroyBody(kv.getValue());
-                        shootersForRemove.add(kv.getKey());
-                        shootersDestroyed.write(kv.getKey(), () ->{
-                            gameShared.shooters.free(kv.getKey());
-                        });
+                    List<Integer> shootersForRemove = new ArrayList<>();
+                    for (Map.Entry<Integer, Body> kv : shooters.entrySet()) {
+                        if (Math.abs(attackY - kv.getValue().getPosition().y) < 1) {
+                            world.destroyBody(kv.getValue());
+                            shootersForRemove.add(kv.getKey());
+                            shootersDestroyed.write(kv.getKey(), () -> {
+                                gameShared.shooters.free(kv.getKey());
+                            });
+                        }
                     }
+                    shootersForRemove.forEach((id) -> shooters.remove((Object) id));
                 }
-                shootersForRemove.forEach((id) -> shooters.remove((Object) id));
-            }
+            }, 300);
 
         }
 
@@ -333,20 +336,21 @@ public class PhysicsSystemDef extends AbstractSystemDef {
         void createWalker(Integer id) {
             walkers.put(id, BodyFactory.createWalker(world, gameShared.walkers.map(id)));
         }
+
         void createShooter(Integer id) {
             shooters.put(id, BodyFactory.createShooter(world, gameShared.shooters.map(id)));
         }
 
-        void jumperJump(Integer id){
+        void jumperJump(Integer id) {
             Body jBody = jumpers.get(id);
             //possible deleted
-            if(jBody == null)
+            if (jBody == null)
                 return;
-            if(jBody.getLinearVelocity().y != 0)
+            if (jBody.getLinearVelocity().y != 0)
                 return;
 
             Vector2 jVec = null;
-            if(playerBody.getPosition().x > jBody.getPosition().x)
+            if (playerBody.getPosition().x > jBody.getPosition().x)
                 jVec = Constants.JUMP_IMPULSE_PEDO_RIGHT;
             else
                 jVec = Constants.JUMP_IMPULSE_PEDO_LEFT;
@@ -354,37 +358,37 @@ public class PhysicsSystemDef extends AbstractSystemDef {
         }
 
 
-        void walkerGo(Integer id){
+        void walkerGo(Integer id) {
             Body wBody = walkers.get(id);
             Walker walker = gameShared.walkers.map(id);
             //possible removed
-            if(wBody == null)
+            if (wBody == null)
                 return;
-            if(walker.nSteps <= 0){
+            if (walker.nSteps <= 0) {
                 walker.nSteps = walker.rand.nextInt(Walker.MAX_STEPS);
-                if(walker.rand.nextInt() % 2 == 0)
+                if (walker.rand.nextInt() % 2 == 0)
                     walker.dirX = DirectionX.LEFT;
                 else
                     walker.dirX = DirectionX.RIGHT;
             }
             walker.nSteps--;
             Vector2 wVec = null;
-            if(walker.dirX ==  DirectionX.LEFT)
+            if (walker.dirX == DirectionX.LEFT)
                 wVec = Constants.WALKING_FORCE_LEFT;
             else
                 wVec = Constants.WALKING_FORCE_RIGHT;
 
-            wBody.applyLinearImpulse(wVec.cpy().scl(0.2f), new Vector2(0,0), true);
+            wBody.applyLinearImpulse(wVec.cpy().scl(0.2f), new Vector2(0, 0), true);
         }
 
-        void shooterShoot(Integer id){
+        void shooterShoot(Integer id) {
             Body sBody = shooters.get(id);
             //possible deleted
-            if(sBody == null)
+            if (sBody == null)
                 return;
 
             Vector2 sVec = null;
-            if(playerBody.getPosition().x > sBody.getPosition().x)
+            if (playerBody.getPosition().x > sBody.getPosition().x)
                 sVec = Constants.SHOOT_IMPULSE_RIGHT;
             else
                 sVec = Constants.SHOOT_IMPULSE_LEFT;
@@ -404,7 +408,7 @@ public class PhysicsSystemDef extends AbstractSystemDef {
                 bottleDestroyed.write(ptr, () -> {
                     gameShared.bottles.free(ptr);
                 });
-            },1000);
+            }, 1000);
         }
     }
 }
